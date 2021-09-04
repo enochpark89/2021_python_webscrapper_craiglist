b2a812d7120cf36c40c2c68a5a2216b62a0fb7d6
Enoch Park <44038883+enochpark89@users.noreply.github.com>
2021-09-03 20:35:29 -0700

    20210903-query craiglist data
    This script scrap 5 different data from the Craiglist

--- craiglist.py
+++ craiglist.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 CRAIGLIST_URL = "https://losangeles.craigslist.org/d/appliances/search/ppa"
+CRAIGLIST_SUB = "";
 
 def extract_craiglist_pages():
         
@@ -37,9 +38,35 @@ def extract_craiglist_items():
    soup = BeautifulSoup(result.text, 'html.parser')
    search_container = soup.find("ul", {"class":"rows"})
    result_rows = search_container.find_all('li')
+
+   arr_of_items = []
+
    # Iterate each result_row
    for result in result_rows:
-       print(result.find("a").find("span"))
-       print(result.findAll("a"))
+       # new query of HTML elements on the sub_page.
+       CRAIGLIST_SUB= result.find("a")['href']
+       sub_result = requests.get(CRAIGLIST_SUB)
+       sub_soup = BeautifulSoup(sub_result.text, 'html.parser')
+       
 
-   return result_rows
+       # From top to bottom - scrap img URL, Title(heading), date, location, price
+       try:
+           image_url = sub_soup.find('img')['src']
+           
+       except:
+            image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png"
+       title = result.find("div", {"class":"result-info"}).find("h3").text
+       date = result.find("div", {"class":"result-info"}).find("time")['datetime']
+       location = result.find("div", {"class":"result-info"}).find("span",{"class":"result-hood"}).text
+       price = result.find("div", {"class":"result-info"}).find("span",{"class":"result-price"}).text 
+       
+       # remove whitespaces from the data
+       image_url= image_url.strip()
+       title = title.strip()
+       date = date.strip()
+       location = location.strip()
+       price = price.strip()
+
+       print(image_url, title, date, location, price)
+
+#    return result_rows
